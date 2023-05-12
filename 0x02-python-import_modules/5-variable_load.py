a592c49542d28b13933aef867dd0fb9019a7e806
christophersesugh <christohybrid185@gmail.com>
2023-05-12 00:03:24 +0000

    ll

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-import variable_load_5
-print(variable_load_5.a)
+if __name__ == "__main__":
+    '''Prints varible a which imported from variable_load_5'''
+    from variable_load_5 import a
 
+    print("{}".format(a))
