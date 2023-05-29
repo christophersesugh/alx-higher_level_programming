e1d6deaff8a0391bee8b472a8839893c277d595a
christophersesugh <christohybrid185@gmail.com>
2023-05-29 23:56:39 +0000

    fix 2

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,12 +7,10 @@ def safe_print_division(a, b):
         result = num / den
     except ZeroDivisionError:
         return None
+    except TypeError:
+        return None
     except:
         return None
     finally:
-        print("Inside result: {}".format(result) if result in locals() else 
-                "Inside result:None")
-        if 'result' in locals():
-            return result
-        else:
-            return None
+        print("Inside result: {}".format(result))
+        return result
