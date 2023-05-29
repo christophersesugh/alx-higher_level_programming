3999fc66181aa73f9dc10d069b32552cac859cea
christophersesugh <christohybrid185@gmail.com>
2023-05-29 23:46:35 +0000

    fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,9 +6,9 @@ def safe_print_division(a, b):
     try:
         result = num / den
     except ZeroDivisionError:
-        return None
+        return result
     except:
-        return None
+        return result
     finally:
-        print("Inside result: {}".format(result))
+        print("Inside result: {:d}".format(result))
         return result
