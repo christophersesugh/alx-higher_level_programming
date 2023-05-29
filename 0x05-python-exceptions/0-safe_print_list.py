a3950281d3142412191f9169fcc053d8016e8789
christophersesugh <christohybrid185@gmail.com>
2023-05-29 23:02:27 +0000

    initial

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0;
-        for element in my_list:
-            if count < x:
-                print(element, end="")
-                count += 1
-        print()
-        return count
-    except TypeError:
-        print("Error: list not iterable")
-        return 0
+    y = 0
+    if not my_list:
+        print("")
+        return (0)
+    while y < x:
+        try:
+            print("{}".format(my_list[y]), end="")
+            y = y + 1
+        except IndexError:
+            break
+    print("")
+    return (y)
