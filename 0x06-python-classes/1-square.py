9b70806792eacc84663224ba1eccfa8afca30512
christophersesugh <christohybrid185@gmail.com>
2023-05-31 12:48:47 +0000

    refactor: add docs to task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Define a class square."""
+
+
 class Square:
-    """Class Square that defines a square object
-    """
+    """Class Square that defines a square object."""
     def __init__(self, size):
         """Initialize method that stores the size of the square
 
