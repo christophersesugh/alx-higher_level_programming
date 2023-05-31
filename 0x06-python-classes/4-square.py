57f35bace20d9db0d7bc2475e81a3f95b3d6543a
christophersesugh <christohybrid185@gmail.com>
2023-05-31 14:31:05 +0000

    fix task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
     def size(self):
         """Retrieve the size of the square."""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """Set the size of the square
