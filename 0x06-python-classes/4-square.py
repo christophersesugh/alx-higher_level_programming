49071722d921a6188889438fe282e212736fbedc
christophersesugh <christohybrid185@gmail.com>
2023-05-31 14:28:53 +0000

    feat: comp. 0x06 task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """Set the size of the square
+
+        Args:
+            value (int): new size for the square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square class."""
+        return self.__size ** 2
