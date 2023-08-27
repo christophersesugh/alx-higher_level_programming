875cff7bae2f88b4d772b6dc49304c4469ed0132
christophersesugh <christohybrid185@gmail.com>
2023-08-26 22:32:33 -0700

    fix 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-// Handle arguments with Javascript
+const argsCount = process.argv.length - 2;
 
-if (process.argv.length < 3) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
+if (argsCount === 0) {
+  console.log("No argument");
+} else if (argsCount === 1) {
+  console.log("Argument found");
 } else {
-  console.log('Arguments found');
+  console.log("Arguments found");
 }
+
